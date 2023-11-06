0213612cd2aa461918c386c802038bdf4f137733
KhoaSN <khoasinno@gmail.com>
2023-11-06 20:13:02 +0700

    #11.6 JWT Middleware - Kiểm Tra Quyền Hạn User

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { toast } from 'react-toastify'
 const instance = axios.create({
     baseURL: 'http://localhost:8081'
 });
@@ -26,7 +26,47 @@ instance.interceptors.response.use(function (response) {
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
+    const status = error.response?.status || 500;
+    switch (status) {
+        // authentication (token related issues)
+        case 401: {
+            toast.error('Unauthorized the user. Please login...')
+            // window.location.href = '/login'
+            return Promise.reject(error);
+        }
+
+        // forbidden (permission related issues)
+        case 403: {
+            toast.error('You don\'t have permission to this access resource...')
+            return Promise.reject(error);
+        }
+
+        // bad request
+        case 400: {
+            return Promise.reject(error);
+        }
+
+        // not found
+        case 404: {
+            return Promise.reject(error);
+        }
+
+        // conflict
+        case 409: {
+            return Promise.reject(error);
+        }
+
+        // unprocessable
+        case 422: {
+            return Promise.reject(error);
+        }
+
+        // generic api error (server related) unexpected
+        default: {
+            return Promise.reject(error);
+        }
+    }
+
 });
 
 export default instance
