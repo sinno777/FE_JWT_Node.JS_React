b620d421f52ca660b3517f0bb98e28ec9a881446
KhoaSN <khoasinno@gmail.com>
2023-11-13 17:32:04 +0700

    #12.4 Add Roles

--- src/components/Role/Roles.js
+++ src/components/Role/Roles.js
@@ -2,29 +2,67 @@ import React, { useEffect, useState } from 'react'
 import './Roles.scss'
 import { v4 as uuidv4 } from 'uuid'
 import _ from 'lodash'
-
+import { toast } from 'react-toastify';
+import { createNewRoles } from '../../services/roleService'
 export default function Roles() {
+    const childDefault = { url: '', description: '', isValidUrl: true }
     const [listChild, setListChild] = useState({
-        child1: { url: '', description: '' }
+        child1: childDefault
     });
 
     const handleOnchangeInput = (name, value, key) => {
         let _listChild = _.cloneDeep(listChild)
         _listChild[key][name] = value
+        if (value && name === 'url') {
+            _listChild[key]['isValidUrl'] = true
+        }
         setListChild(_listChild)
     }
     const handleAddNewInput = () => {
         let _listChild = _.cloneDeep(listChild)
-        _listChild[`child-${uuidv4()}`] = {
-            url: '', description: ''
-        }
+        _listChild[`child-${uuidv4()}`] = childDefault
         setListChild(_listChild)
     }
-    useEffect(() => {
-        Object.entries(listChild).map(([key, child], i) => {
-            console.log([key, child.url, child.description])
+    const handleDelete = (key) => {
+        toast.success('Delete success')
+        let _listChild = _.cloneDeep(listChild)
+        delete _listChild[key]
+        setListChild(_listChild)
+    }
+    const handleSave = async () => {
+        let invalidObj = Object.entries(listChild).find(([key, child], i) => {
+            return child && !child.url // return child if child's url is null
         })
-    });
+        if (!invalidObj) {
+            //cal api
+            let data = buildDataPersist()
+            console.log('>>>>>Check data to build: ', data)
+            let res = await createNewRoles(data)
+            if (res && +res.EC === 0) {
+                toast.success(res.EM)
+            } else {
+                toast.error(res.EM)
+            }
+        } else {
+            toast.error('Input URL must not be empty!')
+            let _listChild = _.cloneDeep(listChild)
+            let key = invalidObj[0]
+            _listChild[key]['isValidUrl'] = false
+            setListChild(_listChild)
+        }
+    }
+    const buildDataPersist = () => {
+        let _listChild = _.cloneDeep(listChild)
+        let result = []
+        Object.entries(_listChild).map(([key, child], i) => {
+            result.push({
+                url: child.url,
+                description: child.description
+            })
+        })
+        return result
+    }
+
     return (
         <div className='role-container'>
             <div className="container">
@@ -37,7 +75,7 @@ export default function Roles() {
                                     <div className={`col-5 form-group ${key}`}>
                                         <label >Url:</label>
                                         <input type="text"
-                                            className='form-control'
+                                            className={child.isValidUrl ? 'form-control' : 'form-control is-invalid'}
                                             value={child.url}
                                             onChange={(e) => handleOnchangeInput('url', e.target.value, key)}
                                         />
@@ -56,14 +94,16 @@ export default function Roles() {
                                             <i className="fa fa-plus-circle text-success fs-2"
                                                 onClick={() => handleAddNewInput()}
                                             ></i>}
-                                        {i >= 1 && <i className="fa fa-trash-o text-danger fs-2" ></i>}
+                                        {i >= 1 && <i className="fa fa-trash-o text-danger fs-2"
+                                            onClick={() => handleDelete(key)}
+                                        ></i>}
 
                                     </div>
                                 </div>
                             )
                         })}
                         <div>
-                            <button className='btn btn-primary mt-3'>Save</button>
+                            <button className='btn btn-primary mt-3' onClick={() => handleSave()}>Save</button>
                         </div>
                     </div>
                 </div>
